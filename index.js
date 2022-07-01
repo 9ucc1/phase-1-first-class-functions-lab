ee81006daab876f409832e0fb10f49036b923be3
Gucci Fan <gucc@umich.edu>
2022-07-01 18:53:48 -0400

    commit tree 0d5eb245e9e0eb21ffdda476b1c5a2d475ada1e8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2)
+}
+const returnLastTwoDrivers = function(array2){
+    return array2.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplyMe){
+    return function multiplier(fare){
+       return multiplyMe * fare
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array, func){
+    if (func === returnFirstTwoDrivers)
+        return returnFirstTwoDrivers(array)
+    else (func === returnLastTwoDrivers)
+        return returnLastTwoDrivers(array)
+}
